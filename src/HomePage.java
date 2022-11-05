7f25d221e593dc9d4c98836464b85e220349ee4a
Mine Dumanogullari <minedumanogullari@Mines-MBP.fios-router.home>
2022-11-05 01:37:50 -0400

    second attempt

--- src/HomePage.java
+++ src/HomePage.java
@@ -2,5 +2,6 @@ public class HomePage {
     public static void main(String[] args) {
         System.out.println("HOMEPAGE");
         System.out.println("Welcome to the HOMEPAGE");
+        System.out.println("Welcome to the HOMEPAGE");
     }
 }
